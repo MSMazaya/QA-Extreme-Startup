e0cd177e115ede818dff12ef770a42ca598a01ac
Matthew Ho <18333157+mehtho@users.noreply.github.com>
2024-12-02 16:45:37 +0000

    Endpoint tests

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -1,6 +1,7 @@
 package com.develogical;
 
 import org.junit.Test;
+import org.junit.Ignore;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
@@ -23,5 +24,15 @@ public class QueryProcessorTest {
     @Test
     public void knowsTeamName() throws Exception {
         assertThat(queryProcessor.process("What is your name?"), containsString("STM33"));
+    }
+
+    @Test
+    public void canDoPlus() throws Exception {
+        assertThat("What is 16 plus 79?", containsString("95"));
+    }
+
+    @Test
+    public void canDoMax() throws Exception {
+        assertThat("Which of the following numbers is the largest: 38, 2, 11?", containsString("38"));
     }
 }
